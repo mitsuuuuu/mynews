0df339a767082acf20713c6a44c744c8fb6f8c89
s-mitsuda <mits.ad.7.4@gmail.com>
2022-01-10 10:32:49 +0000

    Laravel13kadai

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add') -> middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add') -> middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create'); #課題で追記
     Route::get('profile/edit', 'Admin\ProfileController@edit') -> middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();

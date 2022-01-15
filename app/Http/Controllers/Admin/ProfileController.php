4e78a8aefa2143340b835c35e39990c32097ac17
s-mitsuda <mits.ad.7.4@gmail.com>
2022-01-15 07:41:02 +0000

    Laravel13kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,8 +15,19 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profiles = new Profile;
+      $form = $request->all();
+
+      
+      // データベースに保存する
+      $profiles->fill($form);
+      $profiles->save();
         return redirect('admin/profile/create');
     }
     
